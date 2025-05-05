d78a0a33187899adbfca60563784cc106d2d4570
sweun2 <46338536+sweun2@users.noreply.github.com>
2025-05-05 18:16:48 +0900

    feat: add foreigin key, realationship - user,order,item

--- app/core/models.py
+++ app/core/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 from app.core.database import Base
 
 class Users(Base):
@@ -6,5 +7,28 @@ class Users(Base):
 
     id       = Column(Integer, primary_key=True, index=True)
     username = Column(String(50), unique=True, index=True, nullable=False)
-    email    = Column(String(120), unique=True, index=True, nullable=False)
     hashed_pw= Column(String(128), nullable=False)
+    hashed_pw= Column(String(128), nullable=False)
+    
+    orders    = relationship("Orders", back_populates="user")
+    
+
+class Items(Base):
+    __tablename__ = "items"
+    
+    id       = Column(Integer, primary_key=True, index=True)
+    itemname = Column(String(50), unique=True, index=True, nullable=False)
+    price    = Column(Integer, nullable=False)
+    
+    orders    = relationship("Orders", back_populates="item")
+
+class Orders(Base):
+    __tablename__ = "orders"
+
+    id       = Column(Integer, primary_key=True, index=True)
+    item_id  = Column(Integer, ForeignKey("items.id"), nullable=False, index=True)
+    user_id  = Column(Integer, ForeignKey("users.id"), nullable=False, index=True, unique=True)
+    quantity = Column(Integer, nullable=False)
+    user = relationship("Users", back_populates="orders")
+    item = relationship("Items", back_populates="orders")
+    
