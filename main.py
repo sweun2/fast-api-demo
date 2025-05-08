0b6f3f92ba06044bb7af59a90c8652f824606797
sweun2 <46338536+sweun2@users.noreply.github.com>
2025-05-08 18:19:46 +0900

    feat: add origin cors middleware

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from sqlalchemy.orm import Session
 from app.core.database import Base, engine, SessionLocal
 from app.core.models import Items, Users
 from app.core.middleware import LoggingMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 import logging
 logging.basicConfig(
     level=logging.INFO,
@@ -54,4 +55,18 @@ app = FastAPI(
     lifespan=lifespan,
 )
 app.add_middleware(LoggingMiddleware)
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(api_router, prefix="/api/v1")      
